77ffe6330327877a470484476561695ecea4eacd
hohuynhnhu <hhnhu2003@gmail.com>
2025-11-03 14:19:30 +0700

    KAN-95 create a dealer manager screen

---
+++ app/layouts/dealer-manager-layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dealer-manager-layout">
+    <DealerManagerTopbar />
+    <main class="main-content">
+      <div class="content-wrapper">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import DealerManagerTopbar from '@/components/topbars/dealerMTopbar.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  console.log('DealerManagerTopbar component:', DealerManagerTopbar)
+})
+</script>
