10f4add14b2e747c96d9650b731ac4429064eacb
LeKhang-cn22h <chube02082017@gmail.com>
2025-11-04 21:28:37 +0700

    manage compare car logic

---
+++ app/store/compareStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export const useCompareStore = defineStore('compare', {
+  state: () => ({
+    selectedCars: []
+  }),
+  actions: {
+    addCar(car) {
+      if (!this.selectedCars.find(c => c.id === car.id)) {
+        this.selectedCars.push(car)
+      }
+    },
+    removeCar(id) {
+      this.selectedCars = this.selectedCars.filter(c => c.id !== id)
+    },
+    clearAll() {
+      this.selectedCars = []
+    }
+  }
+})
